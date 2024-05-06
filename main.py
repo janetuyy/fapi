5b47f662658a85032fbc3c20bf24269f653e8dc0
janetuyy <118744357+janetuyy@users.noreply.github.com>
2024-05-06 16:35:39 +0300

    views and params

---
+++ main.py
@@ -0,0 +1,22 @@
+import uvicorn
+from items_views import router as items_router
+from fastapi import FastAPI
+from users.views import router as users_router
+
+app = FastAPI()
+app.include_router(items_router)
+app.include_router(users_router)
+
+
+@app.get("/")
+def hello_index():
+    return {"message": "Hello index!"}
+
+
+@app.get("/hello/")
+def hello(name: str = "World"):
+    return {"message": f"Hello {name}"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
