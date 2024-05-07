9e406f6b726c9fe20067e6ab07c6de50cd254e0f
janetuyy <118744357+janetuyy@users.noreply.github.com>
2024-05-08 01:09:53 +0300

    SQLAlchemy and schemas, CRUD, dependency and views for products

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
+from contextlib import asynccontextmanager
+from core.models import Base, db_helper
 import uvicorn
 from items_views import router as items_router
 from fastapi import FastAPI
 from users.views import router as users_router
+from api_v1 import router as router_v1
+from core.config import settings
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
 app.include_router(items_router)
 app.include_router(users_router)
 
