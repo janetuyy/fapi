6a1ada845d0c9ab1229c040e47212c2207e58efe
janetuyy <118744357+janetuyy@users.noreply.github.com>
2024-05-13 12:13:18 +0300

    PostgreSQL

--- core/config.py
+++ core/config.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 from pydantic.v1 import BaseSettings
 
-BASE_DIR = Path(__file__).parent.parent
+# BASE_DIR = Path(__file__).parent.parent
 
 
 class Settings(BaseSettings):
-    db_url: str = f"postgresql://postgres:postgres@localhost:5432/TestFAapp"
+    db_url: str = f"postgresql+asyncpg://postgres:postgres@localhost:5432/TestFAapp"
     db_echo: bool = True
 
 
